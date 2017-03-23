b38a65718c73c566ab41d2fc3e0f09acaa2152ec
lmp <dev.larpon@gmail.com>
2017-03-23 11:28:28 +0100

    Urgh. Remember to include the files

---
+++ App/platforms/android/src/com/blackgrain/android/firebasetest/Main.java
@@ -0,0 +1,23 @@
+package com.blackgrain.android.firebasetest;
+
+import org.qtproject.qt5.android.bindings.QtApplication;
+import org.qtproject.qt5.android.bindings.QtActivity;
+
+import android.util.Log;
+import android.os.Bundle;
+
+import android.view.WindowManager;
+
+public class Main extends QtActivity {
+
+    /** Called when the activity is first created. */
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+    }
+
+}
